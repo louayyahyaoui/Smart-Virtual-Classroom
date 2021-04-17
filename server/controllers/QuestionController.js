441185c89a83a30da38382b41a1ecf864678c145
jmaiiomar <60675567+jmaiiomar@users.noreply.github.com>
2021-04-18 00:45:16 +0200

    update upload file

--- server/controllers/QuestionController.js
+++ server/controllers/QuestionController.js
@@ -1,9 +1,19 @@
 const answer = require("../models/answer");
 var Question = require("../models/question");
 const multer = require("multer");
-const mongoose = require("mongoose");
+const { Storage } = require("@google-cloud/storage");
 
-var storage = multer.diskStorage({
+const mongoose = require("mongoose");
+const perf = require("execution-time")();
+
+const storages = new Storage({
+  projectId: "smart-closer",
+  keyFilename: "./smart-closer-firebase-adminsdk-75ops-25473d0d1e.json",
+});
+
+const bucket = storages.bucket("gs://smart-closer.appspot.com");
+
+/*var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/file/");
   },
@@ -19,13 +29,68 @@ var storage = multer.diskStorage({
   },
 });
 
-var upload = multer({ storage: storage }).single("file");
+var upload = multer({ storage: storage }).single("file");*/
+
 module.exports = {
+  uploads: async (req, res, next) => {
+    const reqFiles = [];
+    console.log(req.files);
+    try {
+      perf.start();
+      for (let i = 0; i < req.files.length; i++) {
+        if (!req.files[i]) {
+          return;
+        }
+
+        // Create new blob in the bucket referencing the file
+        const blob = bucket.file(req.files[i].originalname);
+
+        // Create writable stream and specifying file mimetype
+        const blobWriter = blob.createWriteStream({
+          metadata: {
+            contentType: req.files[i].mimetype,
+          },
+        });
+
+        blobWriter.on("error", (err) => next(err));
+        {
+          blobWriter.on("finish", async () => {
+            // Assembling public URL for accessing the file via HTTP
+
+            await reqFiles.push(
+              `https://firebasestorage.googleapis.com/v0/b/${
+                bucket.name
+              }/o/${encodeURI(blob.name)}?alt=media`
+            );
+
+            // Return the file name and its public URL
+
+            if (i + 1 === req.files.length) {
+              const results = perf.stop();
+              console.log(results.time);
+              setTimeout(() => {
+                res.status(200).send(reqFiles);
+              }, results.time);
+            }
+
+            // console.log(results.time);
+          });
+        }
+
+        // When there is no more data to be consumed from the stream
+        blobWriter.end(req.files[i].buffer);
+      }
+    } catch (error) {
+      // ;
+      return res.status(400).send(`Error, could not upload file: ${error}`);
+    }
+  },
+
   getAnswerAndQuestion: async (req, res, next) => {
     try {
       const { id } = req.params.id;
       const answers = await Question.aggregate([
-    { $match: { Class:  mongoose.Types.ObjectId(req.params.id)} },
+        { $match: { Class: mongoose.Types.ObjectId(req.params.id) } },
         {
           $lookup: {
             from: "answers",
@@ -34,7 +99,7 @@ module.exports = {
             as: "Question_Answer",
           },
         },
-        
+
         { $sort: { "questions.Date": 1 } },
       ]);
       await Question.populate(answers, { path: "Writerq" });
@@ -84,7 +149,9 @@ module.exports = {
   },
 
   addQuestion: async (req, res) => {
+    const file = uploads(req, res);
     const Questionn = req.body;
+    Questionn.Filee = file;
     const newQuestion = new Question(Questionn);
     try {
       const data = await newQuestion.save();
--- server/routes/question.js
+++ server/routes/question.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const {getAllQuestion,addQuestion,uploadFile, getOneQuestion,updateQuestion,deletQuestion,getAnswerAndQuestion,findquestionbyTags} =require('../controllers/QuestionController');
+const {getAllQuestion,addQuestion,uploadFile, getOneQuestion,updateQuestion,deletQuestion,getAnswerAndQuestion,findquestionbyTags,uploads} =require('../controllers/QuestionController');
+
+///UPLOAD FILE 
+const { Storage } = require("@google-cloud/storage");
+const perf = require("execution-time")();
+
+
+// Create new storage instance with Firebase project credentials
+const storages = new Storage({
+  projectId: "smart-closer",
+  keyFilename: "./smart-closer-firebase-adminsdk-75ops-25473d0d1e.json",
+});
+
+// Create a bucket associated to Firebase storage bucket
+const bucket = storages.bucket("gs://smart-closer.appspot.com");
+
+// Initiating a memory storage engine to store files as Buffer objects
+const uploader = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 300 * 1024 * 1024, // limiting files size to 5 MB
+  },
+});
+
+var upload = uploader.array("multiple_resources", 6);
 
 //GET ALL
 router.get('/all/',getAllQuestion);
@@ -12,7 +36,7 @@ router.get('/:id',getOneQuestion);
 //ADD NEW QUESTION
 router.post('/add/',addQuestion);
 //UPLOAD FILE
-router.post('/upload/',uploadFile);
+router.post('/upload/',uploader.array("files", 6),uploads);
 //UPDATE QUESTION 
 router.put('/update/:id',updateQuestion);
 // DELETE QUESTION 
