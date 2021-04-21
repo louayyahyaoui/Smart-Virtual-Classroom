918bb82cbd2220ac32f06ebd92d717f6acf66a50
louayyahyaoui <louay.yahyaoui@esprit.tn>
2021-04-21 04:59:24 +0100

    Fixing reactTimeAgo

--- client/src/Components/sideBare/sideBareComponent.js
+++ client/src/Components/sideBare/sideBareComponent.js
@@ -4,7 +4,6 @@ import { toast } from "react-toastify";
 import {
   Card,
   Image,
-  Button,
   Icon,
   Message,
   Menu,
@@ -28,7 +27,6 @@ import ReactTimeAgo from "react-time-ago/commonjs/ReactTimeAgo";
 const ENDPOINT = "https://closer-server.herokuapp.com/";
 function SideBareComponent() {
   const socket = io(ENDPOINT);
-  const dispatchh = useDispatch();
 
   const state = useSelector((state) => state.user.userUpdated);
   const dispatch = useDispatch();
@@ -45,33 +43,9 @@ function SideBareComponent() {
       SetActiveItem(name);
     }
   };
-  // useEffect(() => {
-  //   dispatch(fetchNotifications(user._id));
-  // }, [dispatch]);
+
   const [nbrNotif, setNbrNotif] = useState(0);
-  const [notifications, errr] = useSelector(selectNotifications);
-
-  // useEffect(() => {
-  //   let i = 0;
-  //   notifications.forEach((element) => {
-  //     if (!element.status) {
-  //       i += 1;
-  //       setNbrNotif(i);
-  //     }
-  //   });
-  //   socket.on("new-notification", (content) => {
-  //     content.forEach((i) => {
-  //       dispatch(fetchNotifications(i));
-  //     });
-
-  //     notifications.forEach((element) => {
-  //       if (!element.status) {
-  //         i += 1;
-  //         setNbrNotif(i);
-  //       }
-  //     });
-  //   });
-  // }, [socket]);
+  const [notifications] = useSelector(selectNotifications);
 
   useEffect(() => {
     dispatch(fetchNotifications(user._id));
@@ -161,43 +135,6 @@ function SideBareComponent() {
                         </Link>
                       )}
                       <Dropdown.Divider />
-                      {notif.Question !== null && (
-                        <Link
-                          to={"/FAQ/" + notif.Question}
-                          onClick={() => updatenotification(notif._id)}
-                        >
-                          <List divided>
-                            <List.Item>
-                              <List.Icon
-                                name="question circle outline"
-                                size="large"
-                                verticalAlign="middle"
-                              />
-
-                              <List.Content>
-                                {notif.status === false ? (
-                                  <List.Header as="p" style={{ color: "blue" }}>
-                                    {notif.Message}
-                                  </List.Header>
-                                ) : (
-                                  <List.Header as="p">
-                                    {notif.Message}
-                                  </List.Header>
-                                )}
-                                <List.Description>
-                                  <p style={{ fontSize: "13px" }}>
-                                    <ReactTimeAgo
-                                      date={notif.Date}
-                                      locale="en-US"
-                                    />{" "}
-                                  </p>{" "}
-                                </List.Description>
-                              </List.Content>
-                            </List.Item>
-                          </List>
-                          <Divider />
-                        </Link>
-                      )}
                     </div>
                   ))}
                 </Dropdown.Menu>
