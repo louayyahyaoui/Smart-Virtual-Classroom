fc7dc70f208376daa940878699fc2e9de08be686
jmaiiomar <60675567+jmaiiomar@users.noreply.github.com>
2021-04-28 09:43:02 +0200

    fix css

--- client/src/Components/authentification/ResetPassword.js
+++ client/src/Components/authentification/ResetPassword.js
@@ -75,6 +75,8 @@ const ResetPassword = ({ match }) => {
                   value={password2}
                 />
                 <button
+                                                    style={{backgroundColor:"red"}}
+
                   type="submit"
                   className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
                 >
