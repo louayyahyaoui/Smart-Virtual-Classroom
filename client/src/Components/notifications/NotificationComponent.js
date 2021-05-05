8eb4ea90010e34e22397bddba6f738077be2e301
jmaiiomar <60675567+jmaiiomar@users.noreply.github.com>
2021-05-05 04:32:24 +0200

    notifcation updated

--- client/src/Components/notifications/NotificationComponent.js
+++ client/src/Components/notifications/NotificationComponent.js
@@ -27,15 +27,14 @@ function NotificationComponent() {
       alert(error);
     }
   };
-const deleted= async(id)=>{
+  const deleted = async (id) => {
     try {
-        const res = await notificationsApi.deleteNotification(id);
-        dispatch(fetchNotifications(user._id));
-  
-      } catch (error) {
-        alert(error);
-      }
-}
+      const res = await notificationsApi.deleteNotification(id);
+      dispatch(fetchNotifications(user._id));
+    } catch (error) {
+      alert(error);
+    }
+  };
   return (
     <Segment raised color="red">
       <Header as="h2" icon textAlign="center">
@@ -50,45 +49,49 @@ const deleted= async(id)=>{
                 {notif.status === false ? (
                   <Message style={{ backgroundColor: "#ADD8E6" }}>
                     <List.Content floated="right">
-                      <Icon name="trash" onClick={()=>deleted(notif._id)}></Icon>
+                      <Icon
+                        name="trash"
+                        onClick={() => deleted(notif._id)}
+                      ></Icon>
                     </List.Content>
                     <Icon name="question circle outline" size="large" />
                     <Link
-                to={"/FAQ/" + notif.Question}
-                onClick={() => updatenotification(notif._id)}
-              >
-                {" "}
-                    <List.Content>{notif.Message}</List.Content>
-                    <List.Description>
-                      <p style={{ fontSize: "13px" }}>
-                        <ReactTimeAgo date={notif.Date} locale="en-US" />{" "}
-                      </p>{" "}
-                    </List.Description>
+                      to={"/FAQ/" + notif.Question}
+                      onClick={() => updatenotification(notif._id)}
+                    >
+                      {" "}
+                      <List.Content>{notif.Message}</List.Content>
+                      <List.Description>
+                        <p style={{ fontSize: "13px" }}>
+                          <ReactTimeAgo date={notif.Date} locale="en-US" />{" "}
+                        </p>{" "}
+                      </List.Description>
                     </Link>
-
                   </Message>
-                ) 
-                : (
+                ) : (
                   <Message>
                     <List.Content floated="right">
-                    <Icon name="trash" onClick={()=>deleted(notif._id)}></Icon>
+                      <Icon
+                        name="trash"
+                        onClick={() => deleted(notif._id)}
+                      ></Icon>
                     </List.Content>
                     <Icon name="question circle outline" size="large" />
                     <Link
-                to={"/FAQ/" + notif.Question}
-                onClick={() => updatenotification(notif._id)}
-              >
-                {" "}
-                    <List.Content>{notif.Message}</List.Content>
-                    <List.Description>
-                      <p style={{ fontSize: "13px" }}>
-                        <ReactTimeAgo date={notif.Date} locale="en-US" />{" "}
-                      </p>{" "}
-                    </List.Description>
+                      to={"/FAQ/" + notif.Question}
+                      onClick={() => updatenotification(notif._id)}
+                    >
+                      {" "}
+                      <List.Content>{notif.Message}</List.Content>
+                      <List.Description>
+                        <p style={{ fontSize: "13px" }}>
+                          <ReactTimeAgo date={notif.Date} locale="en-US" />{" "}
+                        </p>{" "}
+                      </List.Description>
                     </Link>
                   </Message>
                 )}
-                </div>
+              </div>
             )}
             {notif.Course !== null && (
               <Link
