f67982bd914f4e21723758dcc0308155406fed01
jmaiiomar <60675567+jmaiiomar@users.noreply.github.com>
2021-05-30 03:27:28 +0200

    login

--- client/src/Components/authentification/Login.js
+++ client/src/Components/authentification/Login.js
@@ -140,7 +140,7 @@ const Login = ({ history }) => {
                   callback={responseFacebook}
                   render={(renderProps) => (
                     <button
-                    style={{backgroundColor:"royalblue	"}}
+                    style={{backgroundColor:"royalblue"}}
 
                       onClick={renderProps.onClick}
                       className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5"
