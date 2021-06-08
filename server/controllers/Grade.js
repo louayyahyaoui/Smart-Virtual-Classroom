96e9d46ffe3c9a47a63e75d6e57bd54cc34c1a70
sofienargoubi <sofien.argoubi@esprit.tn>
2021-06-08 20:39:05 +0200

    Genarating mail API key

--- server/controllers/Grade.js
+++ server/controllers/Grade.js
@@ -48,7 +48,8 @@ module.exports = {
 console.log(newGrade);
     try {
             
-        Grade.updateOne({_id : newGrade.id} ,{grade : newGrade.grade,taskStatus : "Remis",$push :{listReponse : newGrade.listReponse}})   
+        Grade.updateOne({_id : newGrade.id} ,{grade : newGrade.grade,taskStatus : "Remis",$push :{listReponse : newGrade.listReponse}})  
+    
         .then((grade)=>res.json(grade));
  
      } catch (error) {
